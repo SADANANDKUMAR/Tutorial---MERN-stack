016673b1ed24660a2ef1fd8a99d3e5b91a1bd5c4
Sadanand Kumar <sadanandkr9686@gmail.com>
2025-12-09 08:11:10 +0530

    problem-1 and 2

---
+++ JavaScript/Js-Problem.js
@@ -0,0 +1,39 @@
+ // problem - 1
+ // Topic: Variables & Block Scope (var hoisting)
+// Concept: `var` is hoisted with value `undefined`.
+// Problem: Log a `var` before assignment to observe hoisting.
+// Hints: Declare inside function and log before and after assignment.
+// Examples:
+// Input: demoHoisting()
+// Output: undefined then assigned value
+
+function demoHoisting() {
+  // TODO: implement
+
+    console.log(value); // Should log: undefined
+    var value = "assigned value";
+    console.log(value); // Should log: assigned value
+}
+
+// Test cases (uncomment to run after implementation)
+demoHoisting();
+
+// Problem - 2
+
+// Topic: Hoisting concatenation
+// Concept: Hoisted `var` yields `undefined` in string.
+// Problem: Concatenate hoisted variable with a string.
+// Hints: Use `var` and print before  and after assignment.
+// Examples:
+// Input: demoHoistingSentence()
+// Output: "undefined is a great class" then proper value
+
+function demoHoistingSentence() {
+  // TODO: implement 
+    console.log(greeting + " is a great class"); // Should log: "undefined is a great class"
+    var greeting = "JavaScript";
+ //   console.log(greeting + " is a great class"); // Should log: "JavaScript is a great class"
+}
+
+// Test cases (uncomment to run after implementation)
+ demoHoistingSentence();
